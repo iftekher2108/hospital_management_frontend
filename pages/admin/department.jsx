da991335b5f30b16dc2ec3c656b7d6a3c52e6dba
iftekher2108 <parvezkhan21082002@gmail.com>
2025-11-01 16:02:07 +0600

    now need to handle create and update in one model

--- pages/admin/department.jsx
+++ pages/admin/department.jsx
@@ -3,13 +3,20 @@ import AuthLayout from "@/components/authLayout"
 import Pagination from "@/components/pagination"
 import getToken from "@/lib/getToken"
 import { Fetch } from "@/lib/fetch"
-import { useEffect, useState } from "react"
+import { useCallback, useEffect, useState } from "react"
+import { useSearchParams } from "next/navigation"
 export default function Department() {
+    const [model, setModel] = useState(false)
     const [departments, setDepartments] = useState([])
+    const searchParams = useSearchParams()
     // const [page, setPage] = useState(1);
+    const page = searchParams.get('page') || 1;
+    const limit = searchParams.get('limit') || 10;
 
     const [formdata, setFormData] = useState({
+        id: null,
         name: '',
+        picture: null,
         code: '',
         description: '',
         floor: '',
@@ -20,16 +27,13 @@ export default function Department() {
         totalStaff: '',
         totalBeds: '',
         status: '',
-        address: {
-            first_add: '',
-            secound_add: ""
-        }
     })
 
     // 3. Single handler function for all *top-level* fields
     const handleChange = (e) => {
         const { name, value } = e.target;
 
+        console.log(`${name} = `,value)
         // Use the spread operator (...) to keep all existing fields
         // and only update the field matching the input's 'name'
         setFormData(prevData => ({
@@ -52,9 +56,9 @@ export default function Department() {
     //     }));
     // };
 
-    async function getDepartment() {
+    const getDepartment = useCallback(async () => {
         const token = await getToken()
-        const res = await Fetch(`/api/departments`, {
+        const res = await Fetch(`/api/departments?page=${page}&limit=${limit}`, {
             method: "GET",
             headers: {
                 "Content-Type": "application/json",
@@ -63,11 +67,30 @@ export default function Department() {
         })
         const { data } = await res.json()
         setDepartments(data.data)
-    }
+    })
 
     useEffect(() => {
         getDepartment()
-    }, [departments])
+    }, [getDepartment])
+
+    const modelOpen = async (id = null) => {
+        if (id == null) {
+            document.getElementById('department-store').setAttribute('open', true)
+        } else {
+            document.getElementById('department-store').setAttribute('open', true)
+            const token = await getToken();
+            const res = await Fetch(`/api/departments/${id}`, {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": "Bearer " + token
+                }
+            })
+            const data = await res.json()
+            console.log(data)
+
+        }
+    }
 
 
     function handleSubmit() {
@@ -79,34 +102,75 @@ export default function Department() {
         <AuthLayout>
             <div className="flex justify-between">
                 <h2 className="text-xl font-bold">Department</h2>
-                <button onClick={() => document.getElementById('department-store').showModal()} className="btn btn-primary"><i className="fa-solid fa-plus"></i> Add Department</button>
+                <button onClick={() => modelOpen()} className="btn btn-primary"><i className="fa-solid fa-plus"></i> Add Department</button>
             </div>
-
             <dialog id="department-store" className="modal">
-                <div className="modal-box">
-                    <form method="dialog">
-                        <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
-                    </form>
-                    <span className="font-bold py-2 px-4 rounded text-white bg-primary text-lg">Department</span>
-                    <div className="grid grid-cols-2 p-2">
-                        
-                        <div className="col-span-2 form-control my-3">
-                            <label className="floating-label">
-                                <input type="text" placeholder="Medium" className="input w-full input-md input-primary focus:border-0" />
-                                <span>Medium</span>
-                            </label>
+                <div className="modal-box max-w-3/5">
+
+                    <button onClick={() => document.getElementById('department-store').removeAttribute('open')} className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+
+                    <span className="font-bold py-2 px-4  rounded text-white bg-primary text-lg">Department</span>
+                    <div className="p-2 mt-2">
+                        <div className="grid grid-cols-2 gap-2">
+                            <div className="col-span-1 form-control my-2">
+                                <label className="floating-label">
+                                    <input type="text" name="name" onChange={handleChange} className="input w-full input-lg focus:input-primary focus:outline-0" />
+                                    <span>Name <span className="text-error">*</span></span>
+                                </label>
+                                {formdata.name}
+                            </div>
+
+                            <div className="col-span-1 form-control my-2">
+                                <label className="floating-label">
+                                    <input type="text" onChange={handleChange} name="code" className="input w-full input-lg focus:input-primary focus:outline-0" />
+                                    <span>Code <span className="text-error">*</span></span>
+                                </label>
+                                {formdata.code}
+                            </div>
+
+                            <div className="col-span-2 form-control mb-3">
+                                <p className="text-sm">Pick a file</p>
+                                <input type="file" name="picture" className="file-input w-full focus:file-input-primary focus:outline-0" />
+                                <p className="text-xs">Max size 2MB</p>
+                            </div>
+
+                            <div className="col-span-2 form-control mb-2">
+                                <label className="floating-label">
+                                    <textarea type="text" className="textarea w-full textarea-lg textarea-primary focus:outline-0" />
+                                    <span>Description</span>
+                                </label>
+                            </div>
+
+                            <div className="col-span-1 form-control my-2">
+                                <label className="floating-label">
+                                    <input type="email" onChange={handleChange} name="email" className="input w-full input-lg focus:input-primary focus:outline-0" />
+                                    <span>Email <span className="text-error">*</span></span>
+                                </label>
+
+                            </div>
+
+                            <div className="col-span-1 form-control my-2">
+                                <label className="floating-label">
+                                    <input type="number" onChange={handleChange} name="phone" className="input w-full input-lg focus:input-primary focus:outline-0" />
+                                    <span>Phone <span className="text-error">*</span></span>
+                                </label>
+                                {formdata.phone}
+                            </div>
+
+                            <div className="col-span-2 form-control mb-2">
+                                <select defaultValue="Status" name="status" onChange={handleChange} className="select w-full select-primary focus:outline-0">
+                                    <option value={'active'}>Active</option>
+                                    <option value={'inactive'}>Inactive</option>
+                                </select>
+                                {formdata.status}
+                            </div>
+
                         </div>
-
-                        <div className="col-span-2 form-control mb-3">
-                            <label className="floating-label">
-                                <input type="text" placeholder="Medium" className="input w-full input-md input-primary focus:border-0" />
-                                <span>Medium</span>
-                            </label>
+                        <div className="flex justify-end mt-3">
+                            <button className="btn px-8 btn-primary">Submit</button>
                         </div>
-
-                    <button className="btn btn-primary">Submit</button>
-
                     </div>
+
                 </div>
             </dialog>
             <br />
@@ -132,7 +196,7 @@ export default function Department() {
                                 <td>{department.email}</td>
                                 <td>{department.phone}</td>
                                 <td>
-                                    <button className="btn btn-sm btn-info me-1"><i className="fa-solid fa-pen-to-square"></i></button>
+                                    <button onClick={() => modelOpen(department.id)} className="btn btn-sm btn-info me-1"><i className="fa-solid fa-pen-to-square"></i></button>
                                     <button className="btn btn-sm btn-error me-1"><i className="fa-solid fa-trash"></i></button>
 
                                 </td>
